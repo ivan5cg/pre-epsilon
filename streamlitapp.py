de82ee93a1fca06bca8068344d147474f0d4c943
ivan5cg <ivan.carrillo.g5@gmail.com>
2024-07-06 20:32:32 +0200

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1075,8 +1075,8 @@ def create_performance_plot(rendimientos, start_date, end_date):
         [0, 'rgb(165,0,38)'],
         [0.25, 'rgb(215,48,39)'],
         [0.5, 'rgb(244,109,67)'],
-        [1, 'rgb(253,174,97)'],
-        [1.5, 'rgb(26,152,80)']
+        [0.75, 'rgb(253,174,97)'],
+        [1, 'rgb(26,152,80)']
     ]
 
     # Create the scatter plot
