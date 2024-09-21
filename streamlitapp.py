c95f50261d8230de831439f32dfb66d6cc1bb952
ivan5cg <ivan.carrillo.g5@gmail.com>
2024-09-21 20:10:25 +0200

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1459,7 +1459,7 @@ with col1:
     anos_simulacion = st.slider('Years', min_value=1, max_value=50, value=6)
 
 max_simulations = 30000 - (anos_simulacion - 1) * 500
-num_simulaciones = col2.slider('Number of Simulations', min_value=1000, max_value=max_simulations, value=15000)
+num_simulaciones = col2.slider('Number of Simulations', min_value=1000, max_value=max_simulations, value=8000)
 
 with col3:
     aportacion_mensual = st.number_input('Monthly Contribution', min_value=0, step=50, value=1250)
